1001c75b980b111ed0c8f0b471105b072a827745
Hong Yue Wang <hongyuew26@gmail.com>
2022-07-19 21:41:16 -0400

    Heart empties on removal

--- src/components/List/SongList.js
+++ src/components/List/SongList.js
@@ -5,12 +5,172 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
-import {arrayToStorageString, storageStringToArray} from "../../Helper";
-import { useEffect } from "react";
+import { arrayToStorageString, storageStringToArray } from "../../Helper";
+import { useState, useEffect } from "react";
 
 library.add(fas, far);
 
 export default function SongList(props) {
+  const Song = ({
+    id,
+    name,
+    artists,
+    album,
+    track_number,
+    duration_ms,
+    type,
+    preview_url,
+    popularity,
+    href,
+  }) => {
+    const song = {
+      id: id,
+      name: name,
+      artists: artists,
+      album: album,
+      track_number: track_number,
+      duration_ms: duration_ms,
+      type: type,
+      preview_url: preview_url,
+      popularity: popularity,
+      href: href,
+    };
+
+    let search = () => {
+      return storageStringToArray(
+        localStorage.getItem(props.currentUserData.id)
+      ).length > 0
+        ? storageStringToArray(
+            localStorage.getItem(props.currentUserData.id)
+          )?.find((x) => x?.id == id)
+        : undefined;
+    };
+
+    const [songSearch, setSongSearch] = useState(search());
+
+    useEffect(() => {
+      (() => {
+        setSongSearch(search());
+      })();
+    }, [search]);
+
+    function addSongToFavorites(x) {
+      let favorites = storageStringToArray(
+        localStorage.getItem(props.currentUserData.id)
+      );
+      favorites.push(x);
+      localStorage.setItem(
+        props.currentUserData.id,
+        arrayToStorageString(favorites)
+      );
+      console.log("fav:" + x);
+    }
+
+    function removeSongFromFavorites(x) {
+      let favorites = storageStringToArray(
+        localStorage.getItem(props.currentUserData.id)
+      );
+      favorites.splice(favorites.indexOf(x), 1);
+      localStorage.setItem(
+        props.currentUserData.id,
+        arrayToStorageString(favorites)
+      );
+    }
+
+    return (
+      <div className="d-inline-block row justify-content-between gap-3 mb-5">
+        <div
+          className="me-5"
+          style={{ display: "inline-block", width: "40px" }}
+        >
+          <img
+            src={album.images[0]?.url || "no-photo.jpg"}
+            alt="Album cover"
+            className="h-14 w-14 rounded-sm border border-neutral-800"
+            width="40px"
+            length="40px"
+          />
+        </div>
+        <Link
+          to={`/songs/${id}`}
+          style={{ textDecoration: "none", color: "white" }}
+          key={id}
+        >
+          <div
+            className="me-5"
+            style={{
+              display: "inline-block",
+              width: "250px",
+              minWidth: "0",
+              textAlign: "left",
+              whiteSpace: "nowrap",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+            }}
+          >
+            {name}
+          </div>
+        </Link>
+        <Link
+          to={`/artists/${artists[0].id}`}
+          style={{ textDecoration: "none", color: "white" }}
+        >
+          <div
+            className="me-5"
+            style={{
+              display: "inline-block",
+              width: "250px",
+              textAlign: "left",
+              whiteSpace: "nowrap",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+            }}
+          >
+            {artists[0].name}
+          </div>
+        </Link>
+
+        <Link
+          to={`/albums/${album.id}`}
+          style={{ textDecoration: "none", color: "white" }}
+        >
+          <div
+            className="me-5"
+            style={{
+              display: "inline-block",
+              width: "250px",
+              textAlign: "left",
+              whiteSpace: "nowrap",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+            }}
+          >
+            {album.name}
+          </div>
+        </Link>
+        <div
+          className="me-5"
+          style={{ display: "inline-block", width: "20px", textAlign: "left" }}
+        >
+          {songSearch != undefined ? (
+            <FontAwesomeIcon
+              icon="fa-solid fa-heart"
+              onClick={() => {
+                removeSongFromFavorites(song);
+              }}
+            />
+          ) : (
+            <FontAwesomeIcon
+              icon="fa-regular fa-heart"
+              onClick={() => {
+                addSongToFavorites(song);
+              }}
+            />
+          )}
+        </div>
+      </div>
+    );
+  };
   let display = props.songs.map((x) => {
     let {
       id,
@@ -25,8 +185,23 @@ export default function SongList(props) {
       href,
     } = x;
 
+    return (
+      <Song
+        key={id}
+        id={id}
+        name={name}
+        artists={artists}
+        album={album}
+        track_number={track_number}
+        duration_ms={duration_ms}
+        type={type}
+        preview_url={preview_url}
+        popularity={popularity}
+        href={href}
+      />
+    );
 
-    let songSearch = storageStringToArray(localStorage.getItem(props.currentUserData.id)).length > 0 ? storageStringToArray(localStorage.getItem(props.currentUserData.id))?.find(x => x?.id == id) : undefined;
+    /*let songSearch = storageStringToArray(localStorage.getItem(props.currentUserData.id)).length > 0 ? storageStringToArray(localStorage.getItem(props.currentUserData.id))?.find(x => x?.id == id) : undefined;
 
 
     function addSongToFavorites() {
@@ -120,7 +295,7 @@ export default function SongList(props) {
           }
         </div>
       </div>
-    );
+    );*/
   });
   return (
     <Container>
