3d2b9d4c1b1decf3f7830c22626463f876c6af8d
Hong Yue Wang <hongyuew26@gmail.com>
2022-07-20 12:53:53 -0400

    Add favorites to profile

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Container, Row, Col} from "react-bootstrap";
 import SongList from "../components/List/SongList";
+import { storageStringToArray } from "../Helper";
 
 export default function UserProfile(props) {
   console.log(props.currentUserData);
   let { id, display_name, images, followers, type, href } =
     props.currentUserData;
+
   return (
     <Container>
       <Row className="mt-3">
         <Col xs="3">
           <img
-            src={images[0]?.url || "no-photo.jpg"}
+            src={images?.[0]?.url || "no-photo.jpg"}
             className="img-fluid float-start rounded-circle img-thumbnail"
             alt=""
           />
@@ -20,31 +22,15 @@ export default function UserProfile(props) {
           <h1 className="fw-bold" style={{ fontFamily: "Montserrat" }}>
             {display_name}
           </h1>
-          <p className="lead ffs-6">{followers.total.toLocaleString()} followers on Spotify</p>
+          <p className="lead ffs-6">{followers?.total.toLocaleString()} followers on Spotify</p>
         </Col>
       </Row>
       <Row className="mt-5">
         <h3 className="fw-bold">Favorites</h3>
-        <SongList
-          songs={[
-            {
-              id: 1,
-              name: "Song Name",
-              artists: [{ name: "Artist Name" }],
-              album: {
-                name: "Album Name",
-                images: [],
-                track_number: 1,
-                duration_ms: 10000,
-                type: "song",
-                preview_url: "",
-                popularity: 100,
-                href: "",
-              },
-            },
-          ]}
+        { storageStringToArray(localStorage.getItem(props.currentUserData.id))?.length > 0 ?         <SongList
+          songs={storageStringToArray(localStorage.getItem(props.currentUserData.id))}
           currentUserData={props.currentUserData}
-        ></SongList>
+        /> : <p>No favorites yet</p>}
       </Row>
     </Container>
   );
